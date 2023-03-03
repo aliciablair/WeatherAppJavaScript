51cd19631cdadb70e69297261541aeed9a6f0f8a
girlsgotomarz <alicia.n.blair@gmail.com>
2023-03-03 12:54:01 -0600

    Transfer code from codesandbox to VS

---
+++ src/index.js
@@ -0,0 +1,81 @@
+// API Date //
+function formatMainDate(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[dayIndex];
+  return `${day} ${hours}:${minutes}`;
+}
+
+// API Weather Response //
+function displayWeatherCondition(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  let rain = 0;
+  if (response.data.main.rain) {
+    rain = response.data.main.rain;
+  }
+  document.querySelector("#precipitation").innerHTML = rain;
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+
+// API City Input //
+function searchCity(city) {
+  let apiKey = "28f801d6b2a512f4d4b1dff42a81bc32";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+
+function searchLocation(position) {
+  let apiKey = "28f801d6b2a512f4d4b1dff42a81bc32";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+///////////////////////////////
+
+let date = new Date();
+let dateElement = document.querySelector("#date");
+dateElement.innerHTML = formatMainDate(date);
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
+
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
